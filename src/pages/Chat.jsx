c355860e3d5c1d12ec5df5eeb7cbdbb12e99473b
l1me <102348214+dev-lime@users.noreply.github.com>
2025-04-22 20:14:58 +0500

    New chat

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -17,48 +17,48 @@ const chats = [
 	{
 		id: 1,
 		name: "Чат общежития",
-		lastMessage: "Завтра проверка комнат",
+		lastMessage: "Скоро проверка комнат",
 		time: "21:30",
 		unread: 7,
 	},
 	{
 		id: 2,
-		name: "Академическая группа",
-		lastMessage: "Завтра собрание в 14:00",
+		name: "Катя",
+		lastMessage: "Завтра собрание в 13:00",
 		time: "12:30",
 		unread: 2,
 	},
 	{
 		id: 3,
-		name: "Чат по программированию",
-		lastMessage: "Кто разобрал алгоритм?",
+		name: "Пирогов",
+		lastMessage: "Кто дудосит мой дзен?",
 		time: "11:10",
 		unread: 3,
 	},
 	{
 		id: 4,
-		name: "Преподаватель",
-		lastMessage: "Пришлите ваши работы",
+		name: "Осокина",
+		lastMessage: "Перешли группе: ...",
 		time: "10:15",
 		unread: 0,
 	},
 	{
 		id: 5,
-		name: "Куратор группы",
+		name: "Групповой чат с куратором",
 		lastMessage: "Не забудьте про дедлайн!",
 		time: "09:45",
 		unread: 0,
 	},
 	{
 		id: 6,
-		name: "Староста группы",
-		lastMessage: "Расписание изменилось!",
+		name: "Групповой чат",
+		lastMessage: "В педе нет воды, по домам!",
 		time: "08:00",
 		unread: 0,
 	},
 	{
 		id: 7,
-		name: "Одногруппник",
+		name: "Илья",
 		lastMessage: "Привет, есть вопросы по заданию",
 		time: "Вчера",
 		unread: 1,
@@ -66,15 +66,22 @@ const chats = [
 	{
 		id: 8,
 		name: "Профком",
-		lastMessage: "Регистрация на мероприятие открыта",
+		lastMessage: "Регистрация на мероприятие открыта!",
 		time: "Вчера",
-		unread: 5,
+		unread: 999,
 	},
 	{
 		id: 9,
 		name: "Чат выпускников",
 		lastMessage: "Какие выпускники, мы второй курс",
 		time: "20 мая",
+		unread: 0,
+	},
+	{
+		id: 10,
+		name: "Ещё какой-то чат",
+		lastMessage: "Отстаньте, мне лень придумывать",
+		time: "14.10.2022",
 		unread: 0,
 	},
 ];
@@ -231,7 +238,7 @@ export default function Chat() {
 												badgeContent={chat.unread}
 												color="primary"
 												sx={{
-													mr: 1
+													mr: 2
 												}}
 											/>
 										)}
