d534c3c4450bd6b80511e6e7f25cd3a261e8e04e
l1me <102348214+dev-lime@users.noreply.github.com>
2025-04-26 02:13:11 +0500

    Fixed vendor

--- vite.config.js
+++ vite.config.js
@@ -11,19 +11,5 @@ export default defineConfig({
 			"@components": path.resolve(__dirname, "./src/components"),
 			"@context": path.resolve(__dirname, "./src/context")
 		}
-	},
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes('node_modules')) {
-						if (id.includes('firebase')) return 'vendor-firebase';
-						if (id.includes('react')) return 'vendor-react';
-						if (id.includes('@mui')) return 'vendor-mui';
-						return 'vendor';
-					}
-				}
-			}
-		}
 	}
 })
